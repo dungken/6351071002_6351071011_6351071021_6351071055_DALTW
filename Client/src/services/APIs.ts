fb9cb8a8a0be39e4b6552ca16d9acdf0a3b43e34
Oanh Phạm <128332916+oripham@users.noreply.github.com>
2024-12-12 19:33:51 +0700

    Update APIs.ts

--- Client/src/services/APIs.ts
+++ Client/src/services/APIs.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:5289",
+    baseURL: "https://opham414-001-site1.jtempurl.com/",
 });
 
 api.interceptors.request.use((config) => {
