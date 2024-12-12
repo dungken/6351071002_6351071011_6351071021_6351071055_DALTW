ef1c273512133b4a7a13aa11f0ccbd9176598958
Oanh Phạm <128332916+oripham@users.noreply.github.com>
2024-12-12 19:35:31 +0700

    Update CartService.ts

--- Client/src/services/CartService.ts
+++ Client/src/services/CartService.ts
@@ -13,7 +13,7 @@ export const CreateOrderService = async (orderData: any) => {
 
 
     try {
-        const response = await axios.post('http://localhost:5289/api/Order',
+        const response = await axios.post('https://opham414-001-site1.jtempurl.com/api/Order',
             dto
         );
         return response.data;
@@ -25,7 +25,7 @@ export const CreateOrderService = async (orderData: any) => {
 
 export const GetOrderHistoryService = async (userId: string) => {
     try {
-        const response = await axios.get(`http://localhost:5289/api/Order/user/${userId}`);
+        const response = await axios.get(`https://opham414-001-site1.jtempurl.com/api/Order/user/${userId}`);
         return response.data;
     } catch (error) {
         return (error as any).response.data;
